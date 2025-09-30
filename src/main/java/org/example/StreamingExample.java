8954716f71412d093169feab571d6076bac556bc
Orion Papadakis <orion.papadakis@manchester.ac.uk>
2025-09-30 14:40:46 +0300

    Enhance StreamingExample.java with `onPartialThinking` implementation.

--- src/main/java/org/example/StreamingExample.java
+++ src/main/java/org/example/StreamingExample.java
@@ -4,6 +4,7 @@ import dev.langchain4j.data.message.SystemMessage;
 import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.model.chat.request.ChatRequest;
 import dev.langchain4j.model.chat.response.ChatResponse;
+import dev.langchain4j.model.chat.response.PartialThinking;
 import dev.langchain4j.model.chat.response.StreamingChatResponseHandler;
 import dev.langchain4j.model.gpullama3.GPULlama3StreamingChatModel;
 
@@ -15,14 +16,14 @@ public class StreamingExample {
     public static void main(String[] args) {
 
         // Create model
-        Path modelPath = Paths.get("/home/orion/LLMModels/Qwen3-0.6B-f16.gguf");
+        Path modelPath = Paths.get("/home/orion/LLMModels/Qwen3-8B-f16.gguf");
 
         GPULlama3StreamingChatModel model = GPULlama3StreamingChatModel.builder()
                 .modelPath(modelPath)
                 .temperature(0.6)
                 .topP(1.0)
                 .maxTokens(2048)
-                .onGPU(Boolean.FALSE) // if false, runs on CPU though a lightweight implementation of llama3.java
+                .onGPU(Boolean.TRUE) // if false, runs on CPU though a lightweight implementation of llama3.java
                 .build();
 
 
@@ -50,12 +51,19 @@ public class StreamingExample {
         model.chat(request, new StreamingChatResponseHandler() {
 
             @Override
+            public void onPartialThinking(PartialThinking partialThinking) {
+                System.out.print(partialThinking.text());
+            }
+
+            @Override
             public void onPartialResponse(String partialResponse) {
                 System.out.print(partialResponse);
             }
 
             @Override
             public void onCompleteResponse(ChatResponse completeResponse) {
+                System.out.println(); // New line after streaming
+
                 futureResponse.complete(completeResponse);
                 model.printLastMetrics();
             }
